991cac981ed8214d4bb681353bef93f66adeb90f
Merazul Amin Saikut <merazulamin2122@yahoo.com>
2022-12-25 11:34:35 +0600

    router firebase tailwind initialized

---
+++ src/Pages/SharedPages/Navbar/Navbar.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div>
+            {/* two div for logo and nav links */}
+
+            {/* Left Logo */}
+            <div>
+                <div>
+                    <img src="https://d19qjkjk65tfln.cloudfront.net/v2/assets/img/home/bangladesh-railway.png" alt="" />
+                </div>
+                <div>
+                    <h4 className='text-6xl'>Bangladesh Railway</h4>
+                </div>
+
+            </div>
+
+
+            {/* Navlinks */}
+            <div>
+
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
